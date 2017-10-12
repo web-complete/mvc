c14b916a2996d47ee0d4c895785c39e20076b3cb
mkas <mvkasatkin@gmail.com>
2017-10-13 00:39:19 +0700

    dev

--- tests/include/bootstrap.php
+++ tests/include/bootstrap.php
@@ -5,3 +5,7 @@ include_once 'MvcTestCase.php';
 include_once 'SomeController.php';
 include_once 'app/controllers/ErrorController.php';
 include_once 'app/controllers/SomeController.php';
+
+if (!@mkdir(__DIR__ . '/app/packages') && !is_dir(__DIR__ . '/app/packages')) {
+    throw new RuntimeException(sprintf('Directory "%s" was not created', __DIR__ . '/app/packages'));
+}
